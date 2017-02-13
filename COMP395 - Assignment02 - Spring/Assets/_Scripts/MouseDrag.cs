45152abe8d76df9da74d096f796873313eb20125
Faim Khandker <faim.khandker@gmail.com>
2017-02-12 23:20:33 -0500

    Commit 2
    Completed OnDrag and attempting to implement input fields to allow user editing on changing values for the spring constant and mass

---
+++ COMP395 - Assignment02 - Spring/Assets/_Scripts/MouseDrag.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDrag : MonoBehaviour
+{
+	//PUBLIC INSTANCES
+	public float distance = 15f; //Floating variable for distance (z-axis)
+
+//	private Transform _springTransform;
+//
+//	void Start()
+//	{
+//		//References to componens in Unity
+//		this._springTransform = gameObject.GetComponent<Transform>();
+//	}
+
+	//METHODS
+	//Dragging the "spring" object with mouse
+	void OnMouseDrag()
+	{
+		//Instantiating a Vector3 object with x, y, and z axis values.
+		Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+		//
+		Vector3 objPosition = Camera.main.ScreenToWorldPoint (mousePosition);
+
+		//The position of the object is assigned to the Vector3 object,"objPOsition"
+		transform.position = objPosition;
+	}
+
+}
