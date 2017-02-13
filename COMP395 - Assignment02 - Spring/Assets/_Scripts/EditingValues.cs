598bd29455e6cda5dc91bcdd8ec00003a95bb8c0
Faim Khandker <faim.khandker@gmail.com>
2017-02-13 00:38:29 -0500

    Commit 3
    Refined version

---
+++ COMP395 - Assignment02 - Spring/Assets/_Scripts/EditingValues.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EditingValues : MonoBehaviour 
+{
+//	//PUBLIC INSTANCES
+//	public InputField inputSpringConst;
+//	public InputField inputMass;
+	public Rigidbody mass;
+
+	//PRIVATE INSTANCES
+	private InputField _inputSpringConst;
+	private InputField _inputMass;
+//	private Rigidbody _rigidbody;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//Referrencing to GameObjects in Unity
+		_inputSpringConst = GetComponent<InputField>();
+		_inputMass = GetComponent<InputField>();
+		//rigidbody = GetComponent<Rigidbody>();
+	}
+
+	//METHODS
+	//Change Mass via input field, "inputMass"
+	public void EditMass(string newText)
+	{
+		//Converting Float into String
+		float temp = float.Parse(newText);
+
+		//
+//		mass = new 
+	}
+}
---
+++ COMP395 - Assignment02 - Spring/Assets/_Scripts/OutputProperties.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OutputProperties : MonoBehaviour 
+{
+	//PUBLIC INSTANCES
+	public Text textProperties;
+
+	//PRIVATE INSTANCES
+
+	//METHODS
+	//Output both the "Spring Constant" and the "Mass"
+	private void _PropertiesOutput()
+	{
+		//Setting/Updating the text (INCOMPLETE)
+		textProperties.text = "Spring Constant:";
+	}
+
+	//EXTRA
+//	//Resets transform of the base and spring objects
+//	public void Reset()
+//	{
+//		
+//	}
+
+	//Exit
+	public void Exit()
+	{
+		Application.Quit ();
+	}
+}
